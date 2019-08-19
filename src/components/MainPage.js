6f3fc4ce3bd0c7a5ca09847c87a07e5ce945b10c
Roman Pirag <romanpirag@gmail.com>
2019-08-19 15:02:49 -0400

    Creates Main Page Component

---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class MainPage extends React.Component {
+  state = {
+  
+  }
+
+  render() {
+    return <h1>HELLO HELLO HELLO </h1>
+  }
+}
+
+export default MainPage
