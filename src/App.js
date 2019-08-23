9e048fb6d1fd7ebe4dd0a55c6b7600be4bfb6c62
Roman Pirag <romanpirag@gmail.com>
2019-08-22 20:56:10 -0400

    Creates Header Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import MainPage from "./components/MainPage"
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Header/>
       </header>
       <main>
         <MainPage/>
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Header = () => {
+  return <h1>NEONFLIX 2000</h1>
+}
+
+
+export default Header
