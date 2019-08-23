123ad3dd63114ae5b027ce5a6eb56e99eed114de
Roman Pirag <romanpirag@gmail.com>
2019-08-23 00:40:24 -0400

    Changes layout to left aligned

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,7 +4,7 @@ import React from "react"
 const Movie = (props) => {
     return (
       <div className="moviecard">
-        <h2>{props.movie.title}</h2>
+        {/* <h2>{props.movie.title}</h2> */}
         <img alt="" className="image" src={props.movie.image} />
       </div>
     )
