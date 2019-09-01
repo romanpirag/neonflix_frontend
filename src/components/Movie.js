131801b5a0cf7cd22d9f72fdc3603f171f7134b1
Roman Pirag <romanpirag@gmail.com>
2019-08-31 20:20:08 -0400

    Fixes Grid Responsiveness

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,10 +3,7 @@ import React from "react"
 
 const Movie = (props) => {
     return (
-      <div className="moviecard">
-        {/* <h2>{props.movie.title}</h2> */}
         <img alt="" className="image" src={props.movie.image} />
-      </div>
     )
 }
 
