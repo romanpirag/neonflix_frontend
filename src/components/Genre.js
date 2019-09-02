a0f874a9dfccbe8b71d2d088a938257899532e7d
Roman Pirag <romanpirag@gmail.com>
2019-09-02 19:48:49 -0400

    Changes CSS

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -3,7 +3,7 @@ import React from "react"
 const Genre = props => {
   return (
     <button className="genre-button">
-      {props.genreName} <span className="genreCount">{props.genreCount}</span>
+      {props.genreName} 
     </button>
   )
 }
