0f3658cc9a6a03670139251f8dff922b3c658f8b
Roman Pirag <romanpirag@gmail.com>
2019-09-02 20:51:02 -0400

    Adds right side git and linkedin links

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,30 @@ import React from "react"
 
 const Header = () => {
   return (
-    <div className="title-div">
-      <h1 className="colorshift">NEONFLIX</h1>
-      <div className="tagline">1980's Movie Nostalgia</div>
+    <div className="header">
+      <div className="title-div">
+        <h1 className="colorshift">NEONFLIX</h1>
+        <div className="tagline">1980's Movie Nostalgia</div>
+      </div>
+      <div className="right-side-links">
+        <a
+          className="gitlink"
+          href="https://github.com/romanpirag"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-github-square" />
+        </a>
+
+        <a
+          className="linkedin-link"
+          href="https://www.linkedin.com/in/romanpirag/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-linkedin" />
+        </a>
+      </div>
     </div>
   )
 }
