58e6638525480dd26043ffac06b8d817e5194cdb
Roman Pirag <romanpirag@gmail.com>
2019-09-24 00:57:19 -0400

    Changes genre count from an object to an array

--- src/components/GenresContainer.js
+++ src/components/GenresContainer.js
@@ -2,30 +2,27 @@ import React from "react"
 import Genre from "./Genre"
 
 const makeGenres = movies => {
-  const genreCount = {}
+  const genreCount = []
   for (let i = 0; i < movies.length; i++) {
     const movie = movies[i]
     const genre = movie.genre
-    if (genreCount[genre]) {
-      genreCount[genre] += 1
-    } else {
-      genreCount[genre] = 1
-    }
+    genreCount.push(genre)
   }
-  return genreCount
+  const uniqueGenre = [...new Set(genreCount)]
+  return uniqueGenre.sort()
 }
 
 const GenresContainer = props => {
-  const genresCountObject = makeGenres(props.movies)
-
-  const genresArray = Object.keys(genresCountObject).sort()
+  const genresArray = makeGenres(props.movies)
 
   const genresMapped = genresArray.map((genre, index) => {
-    const genreCount = genresCountObject[genre]
-    return <Genre genreName={genre} genreCount={genreCount} key={index} />
+    return <Genre genreName={genre} key={index} />
   })
 
   return <div className="genres-container">{genresMapped}</div>
 }
 
 export default GenresContainer
+
+
+
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,3 +5,4 @@ const Movie = props => {
 }
 
 export default Movie
+
